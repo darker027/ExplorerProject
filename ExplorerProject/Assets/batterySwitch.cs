734ea7e80f8a8b2f04b7b71aad8b2df4e066ddc7
jame-coder <akawin_s@sci.tu.ac.th>
2021-11-26 02:14:33 +0700

    finish battery and pick up

--- ExplorerProject/Assets/batterySwitch.cs
+++ ExplorerProject/Assets/batterySwitch.cs
@@ -9,20 +9,27 @@ public class batterySwitch : MonoBehaviour
     [SerializeField] private Material On;
     [SerializeField] private Material Off;
     [SerializeField] private GameObject electricObject;
+    private bool haveBattery;
     // Start is called before the first frame update
     void Start()
     {
         havePower = false;
+        haveBattery = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if(electricValue > 0 && havePower == true)
+        if(electricValue > 0 && havePower == true && haveBattery == false)
         {
            
             electricValue -= Time.deltaTime; 
+            electricObject.GetComponent<Fan>().setIsPower(havePower);
+            gameObject.GetComponent<Renderer>().material = On;
+        }
+        else if (haveBattery == true)
+        {
             electricObject.GetComponent<Fan>().setIsPower(havePower);
             gameObject.GetComponent<Renderer>().material = On;
         }
@@ -32,14 +39,30 @@ public class batterySwitch : MonoBehaviour
             electricObject.GetComponent<Fan>().setIsPower(havePower);
             gameObject.GetComponent<Renderer>().material = Off;
         }
+        
     }
     private void OnTriggerEnter(Collider other)
     {
-         if (other.gameObject.CompareTag("Electric"))
+         if (other.gameObject.CompareTag("Battery"))
          {
-            //electricValue = other.GetComponent<ElectricAbility>().electric;
-           // Debug.Log(other.GetComponent<ElectricAbility>().electric + "switch");
+            Debug.Log("Enter");
+            haveBattery = true;
+            havePower = true;
         }
+    }
+    //ถ้าสีไม่ยอมเปลี่ยนให้เหี้ยนี่ผิด
+   /* private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Battery"))
+        {
+            electricObject.GetComponent<Fan>().setIsPower(havePower);
+            gameObject.GetComponent<Renderer>().material = On;
+        }
+    }*/
+    private void OnTriggerExit(Collider other)
+    {
+        havePower = false;
+        haveBattery = false;
     }
     public void setValue(float value)
     {
