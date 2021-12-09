cfc540d5c6374818b83162006347f1a8e66e4705
jame-coder <akawin_s@sci.tu.ac.th>
2021-12-09 22:47:07 +0700

    fix button

--- ExplorerProject/Assets/IceSwitch.cs
+++ ExplorerProject/Assets/IceSwitch.cs
@@ -5,22 +5,39 @@ using UnityEngine;
 public class IceSwitch : MonoBehaviour
 {
     public bool IsOn;
+    public bool IsPlayerNear;
+    [SerializeField] Material blue;
     // Start is called before the first frame update
     void Start()
     {
         IsOn = false;
+        IsPlayerNear = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.E) && IsPlayerNear)
+        {
+            IsOn = true;
+        }
+        if(IsOn == true)
+        {
+            this.gameObject.GetComponent<Renderer>().material = blue;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Ice"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            IsOn = true;
+            IsPlayerNear = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IsPlayerNear = false;
         }
     }
 }
