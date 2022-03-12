92a4b0980e94fe5b9b6539878b2932b0aa88e271
jame-coder <akawin_s@sci.tu.ac.th>
2022-03-12 16:27:07 +0700

    add more to main menu

--- ExplorerProject/Assets/eventManager.cs
+++ ExplorerProject/Assets/eventManager.cs
@@ -5,23 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class eventManager : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenuScreen;
+    [SerializeField] private GameObject creditScreen;
+    [SerializeField] private GameObject optionScreen;
     // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void tutorialButton()
     {
         SceneManager.LoadScene("L01",LoadSceneMode.Single);
     }
     public void QuitButton()
     {
-
+            
+    }
+    public void OptionButton()
+    {
+        mainMenuScreen.SetActive(false);
+        optionScreen.SetActive(true);
+    }
+    public void CreditButton()
+    {
+        mainMenuScreen.SetActive(false);
+        creditScreen.SetActive(true);
+    }
+    public void optionBackButton()
+    {
+        mainMenuScreen.SetActive(true);
+        optionScreen.SetActive(false);
+    }
+    public void CreditBackButton()
+    {
+        mainMenuScreen.SetActive(true);
+        creditScreen.SetActive(false);
     }
 }
