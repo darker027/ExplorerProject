97adfbb13fcf532c89f74655b13c4fd6edb1a466
16JuLFriEnD <54356390+darker027@users.noreply.github.com>
2022-03-14 00:01:18 +0700

    put water logic into R3

--- ExplorerProject/Assets/Script/WaterLogic.cs
+++ ExplorerProject/Assets/Script/WaterLogic.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class WaterLogic : MonoBehaviour
 {
-    private bool flowCheck;
+    [SerializeField] private GameObject waterPrefab;
+
+    private float checkDelay;
+
+    private bool downBlocked;
+    private bool forwardBlocked;
+    private bool backwardBlocked;
+    private bool rightBlocked;
+    private bool leftBlocked;
 
     // Start is called before the first frame update
     void Start()
@@ -14,65 +22,106 @@ public class WaterLogic : MonoBehaviour
 
     void Update()
     {
-        //Water logic
-        if(!flowCheck)
+        if(!downBlocked && checkDelay <= 0)
         {
             // Checking flowing downward
             if (Physics.Raycast(gameObject.transform.position, -transform.up, out RaycastHit downHit, 1.25f))
             {
                 // Checking flowing forward
-                if (Physics.Raycast(gameObject.transform.position, transform.forward, out RaycastHit forwardHit, 2.5f))
+                if (!forwardBlocked)
                 {
+                    if (Physics.Raycast(gameObject.transform.position, transform.forward, out RaycastHit forwardHit, 2.5f))
+                    {
+                        if (forwardHit.transform.tag == "Water" || forwardHit.transform.tag == "Platform")
+                        {
+                            forwardBlocked = true;
+                        }
+                    }
+                    else
+                    {
+                        StartCoroutine(WaterGenarating(waterPrefab, transform.position + new Vector3(0, 0, 5)));
+                        forwardBlocked = true;
+                        checkDelay = 1.0f;
+                    }
+                }
 
-                }
-                else
-                {
-                    StartCoroutine(WaterGenarating(this.gameObject, transform.position + new Vector3(0, 0, 5)));
-                }
                 // Checking flowing backward
-                if (Physics.Raycast(gameObject.transform.position, -transform.forward, out RaycastHit backwardHit, 2.5f))
+                if (!backwardBlocked)
                 {
+                    if (Physics.Raycast(gameObject.transform.position, -transform.forward, out RaycastHit backwardHit, 2.5f))
+                    {
+                        if (backwardHit.transform.tag == "Water" || backwardHit.transform.tag == "Platform")
+                        {
+                            backwardBlocked = true;
+                        }
+                    }
+                    else
+                    {
+                        StartCoroutine(WaterGenarating(waterPrefab, transform.position + new Vector3(0, 0, -5)));
+                        backwardBlocked = true;
+                        checkDelay = 1.0f;
+                    }
+                }
 
-                }
-                else
-                {
-                    StartCoroutine(WaterGenarating(this.gameObject, transform.position + new Vector3(0, 0, -5)));
-                }
                 // Checking flowing right
-                if (Physics.Raycast(gameObject.transform.position, transform.right, out RaycastHit rightHit, 2.5f))
+                if (!rightBlocked)
                 {
+                    if (Physics.Raycast(gameObject.transform.position, transform.right, out RaycastHit rightHit, 2.5f))
+                    {
+                        if (rightHit.transform.tag == "Water" || rightHit.transform.tag == "Platform")
+                        {
+                            rightBlocked = true;
+                        }
+                    }
+                    else
+                    {
+                        StartCoroutine(WaterGenarating(waterPrefab, transform.position + new Vector3(5, 0, 0)));
+                        rightBlocked = true;
+                        checkDelay = 1.0f;
+                    }
+                }
 
-                }
-                else
-                {
-                    StartCoroutine(WaterGenarating(this.gameObject, transform.position + new Vector3(5, 0, 0)));
-                }
                 // Checking flowing left
-                if (Physics.Raycast(gameObject.transform.position, -transform.right, out RaycastHit leftHit, 2.5f))
+                if (!leftBlocked)
                 {
-
+                    if (Physics.Raycast(gameObject.transform.position, -transform.right, out RaycastHit leftHit, 2.5f))
+                    {
+                        if (leftHit.transform.tag == "Water" || leftHit.transform.tag == "Platform")
+                        {
+                            leftBlocked = true;
+                        }
+                    }
+                    else
+                    {
+                        StartCoroutine(WaterGenarating(waterPrefab, transform.position + new Vector3(-5, 0, 0)));
+                        leftBlocked = true;
+                        checkDelay = 1.0f;
+                    }
                 }
-                else
+
+                if (forwardBlocked && backwardBlocked && rightBlocked && leftBlocked)
                 {
-                    StartCoroutine(WaterGenarating(this.gameObject, transform.position + new Vector3(-5, 0, 0)));
+                    downBlocked = true;
+                    return;
                 }
-
-                flowCheck = true;
-                return;
             }
             else
             {
-                StartCoroutine(WaterGenarating(this.gameObject, transform.position + new Vector3(0, -2.5f, 0)));
-                flowCheck = true;
+                StartCoroutine(WaterGenarating(waterPrefab, transform.position + new Vector3(0, -2.5f, 0)));
+                downBlocked = true;
+                checkDelay = 1.0f;
                 return;
             }
         }
-
+        else
+        {
+            checkDelay -= Time.deltaTime;
+        }
     }
 
     IEnumerator WaterGenarating(GameObject waterReference, Vector3 waterPosition)
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(0.5f);
         GameObject waterSpawn = Instantiate(waterReference, waterPosition, Quaternion.identity);
     }
 }
