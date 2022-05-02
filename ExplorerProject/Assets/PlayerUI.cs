c5b77b5a6a7735fbf91c5d4a7e67229804d94b5f
16JuLFriEnD <54356390+darker027@users.noreply.github.com>
2022-05-02 14:01:20 +0700

    fix UI bug

--- ExplorerProject/Assets/PlayerUI.cs
+++ ExplorerProject/Assets/PlayerUI.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerUI : MonoBehaviour
 {
     [Header("Objects Reference")]
     [SerializeField] private PlayerMovement playerMovementSC;
+    [SerializeField] private GameObject CameraBase;
 
     [SerializeField] private Slider staminaBar;
     [SerializeField] private TMPro.TextMeshProUGUI cooldownText;
 
+    private void OnEnable()
+    {
+        //find camera On load
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        //if (CameraBase == null)
+
+        CameraBase = GameObject.FindWithTag("cameraBase");
+        staminaBar = CameraBase.GetComponentInChildren<Slider>();
+        cooldownText = CameraBase.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+
         playerMovementSC = this.gameObject.GetComponent<PlayerMovement>();
 
         staminaBar.minValue = 0;
