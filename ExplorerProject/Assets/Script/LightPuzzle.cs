73988e9eefe56ce0a16d8ae7cbfda2f6a0249fcc
16JuLFriEnD <54356390+darker027@users.noreply.github.com>
2022-05-15 17:19:07 +0700

    fix bug

--- ExplorerProject/Assets/Script/LightPuzzle.cs
+++ ExplorerProject/Assets/Script/LightPuzzle.cs
@@ -8,6 +8,7 @@ public class LightPuzzle : MonoBehaviour
     [SerializeField] private int[] requireValue;
 
     private bool complete;
+    private bool Cheat;
 
     // Start is called before the first frame update
     void Start()
@@ -18,30 +19,42 @@ public class LightPuzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < checkPoints.Length; i++)
+        if (Cheat)
         {
-            if (Physics.Raycast(checkPoints[i].position, Vector3.down, out RaycastHit hit, float.MaxValue))
+            complete = true;
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.P))
             {
-                if (hit.transform.TryGetComponent<WaterLogic>(out WaterLogic waterCheck))
-                {
-                    checkPoints[i].GetComponentInChildren<TMPro.TextMeshProUGUI>().enabled = true;
-                    checkPoints[i].GetComponentInChildren<TMPro.TextMeshProUGUI>().text = answerManager.waterArr[waterCheck.waterSerial].ToString();
+                Cheat = true;
+            }
 
-                    if (waterCheck.waterValue >= requireValue[i])
+            for (int i = 0; i < checkPoints.Length; i++)
+            {
+                if (Physics.Raycast(checkPoints[i].position, Vector3.down, out RaycastHit hit, float.MaxValue))
+                {
+                    if (hit.transform.TryGetComponent<WaterLogic>(out WaterLogic waterCheck))
                     {
-                        complete = true;
+                        checkPoints[i].GetComponentInChildren<TMPro.TextMeshProUGUI>().enabled = true;
+                        checkPoints[i].GetComponentInChildren<TMPro.TextMeshProUGUI>().text = answerManager.waterArr[waterCheck.waterSerial].ToString();
+
+                        if (answerManager.waterArr[waterCheck.waterSerial] >= requireValue[i])
+                        {
+                            complete = true;
+                        }
+                        else
+                        {
+                            complete = false;
+                            break;
+                        }
                     }
                     else
                     {
+                        checkPoints[i].GetComponentInChildren<TMPro.TextMeshProUGUI>().enabled = false;
                         complete = false;
                         break;
                     }
-                }
-                else
-                {
-                    checkPoints[i].GetComponentInChildren<TMPro.TextMeshProUGUI>().enabled = false;
-                    complete = false;
-                    break;
                 }
             }
         }
@@ -49,10 +62,12 @@ public class LightPuzzle : MonoBehaviour
         if (!complete)
         {
             this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            this.gameObject.GetComponent<SphereCollider>().enabled = false;
         }
         else
         {
             this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            this.gameObject.GetComponent<SphereCollider>().enabled = true;
         }
     }
 }
